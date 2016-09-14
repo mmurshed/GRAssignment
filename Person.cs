316380cd2f71ea95cbe7110eb5685dc10283abcb
Mahbub Murshed <murshed@gmail.com>
2016-09-13 21:45:01 -0500

    Created person class.

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRAssignment
+{
+  public class Person
+  {
+    public string LastName { get; private set; }
+    public string FirstName { get; private set; }
+    public string Gender { get; private set; }
+    public string FavoriteColor { get; private set; }
+
+    public DateTime DateOfBirth { get; private set; }
+
+    Person(string LastName, string FirstName, string Gender, string FavoriteColor, DateTime DateOfBirth)
+    {
+      this.LastName = LastName;
+      this.FirstName = FirstName;
+      this.Gender = Gender;
+      this.FavoriteColor = FavoriteColor;
+      this.DateOfBirth = DateOfBirth;
+    }
+  }
+}
