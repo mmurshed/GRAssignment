aff208408d170f0f528f222e841c3567e235c2a3
Mahbub Murshed <murshed@gmail.com>
2016-09-13 22:22:27 -0500

    Added compareres.

---
+++ GenderComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRAssignment.DataStructure
+{
+  public class GenderComparer: IComparer<Person>
+  {
+    public int Compare(Person x, Person y)
+    {
+      if (x.Gender == y.Gender)
+        return x.Gender.CompareTo(y.Gender);
+
+      if (x.Gender == GenderType.Female && y.Gender == GenderType.Male)
+        return -1;
+      return 1;
+    }
+  }
+}
