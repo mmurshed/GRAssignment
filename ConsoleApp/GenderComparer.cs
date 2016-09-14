3d763b97e19c6bd253b5e848cfff655c5b8eb2ab
Mahbub Murshed <murshed@gmail.com>
2016-09-14 18:47:00 -0500

    Reorg folder structure.

--- ConsoleApp/GenderComparer.cs
+++ ConsoleApp/GenderComparer.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace GRAssignment.DataStructure
+namespace GRAssignment.ConsoleApp.DataStructure
 {
   public class GenderComparer: IComparer<Person>
   {
