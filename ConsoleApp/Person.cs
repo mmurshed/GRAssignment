115e57ca2b97fe5f92a81632fca1c567b07f3698
Mahbub Murshed <murshed@gmail.com>
2016-09-14 23:04:05 -0500

    Basic unit test

--- ConsoleApp/Person.cs
+++ ConsoleApp/Person.cs
@@ -2,7 +2,7 @@
 
 namespace GRAssignment.ConsoleApp.DataStructure
 {
-  public class Person
+  public class Person : IEquatable<Person>
   {
     public string LastName { get; private set; }
     public string FirstName { get; private set; }
@@ -18,6 +18,19 @@ namespace GRAssignment.ConsoleApp.DataStructure
       this.Gender = Gender.CompareTo(GenderType.Male.ToString()) == 0? GenderType.Male : GenderType.Female;
       this.FavoriteColor = FavoriteColor;
       this.DateOfBirth = DateOfBirth;
+    }
+    public bool Equals(Person other)
+    {
+      if (other == null)
+        return false;
+
+      if (LastName == other.LastName &&
+        FirstName == other.FirstName &&
+        Gender == other.Gender &&
+        FavoriteColor == other.FavoriteColor &&
+        DateOfBirth == other.DateOfBirth)
+        return true;
+      return false;
     }
   }
 
