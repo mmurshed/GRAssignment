93527b14b9c1d59d64ba9530c6e577a8a9b05574
Mahbub Murshed <murshed@gmail.com>
2016-09-17 14:16:40 -0500

    Basic working prototype. No output yet.

---
+++ GRPersonService/PersonService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GRAssignment.GRPersonService
+{
+  public class PersonService : IHttpHandler
+  {
+    bool IHttpHandler.IsReusable
+    {
+        get { return true; }
+    }
+    void IHttpHandler.ProcessRequest(HttpContext context)
+    {
+      try
+      {
+        switch(context.Request.HttpMethod)
+        {
+          case "GET":
+            GetPersons(context);
+            break;
+          case "POST":
+            CreatePerson(context);
+            break;
+        }
+      }
+      catch (Exception ex)
+      {
+        context.Response.Write(ex.Message);
+      }
+    }
+
+    private void CreatePerson(HttpContext context)
+    {
+    }
+
+    private void GetPersons(HttpContext context)
+    {
+    }
+  }
+}
--- GRPersonService/Properties/AssemblyInfo.cs
+++ GRPersonService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GRRESTAPI")]
+[assembly: AssemblyTitle("GRPersonService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GRRESTAPI")]
+[assembly: AssemblyProduct("GRPersonService")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
